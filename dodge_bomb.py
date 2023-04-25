dee96ebaa34299e639cb8ea9d169efeabd08b644
tomoya <c011929257@edu.teu.ac.jp>
2023-04-25 14:12:31 +0900

    ランダムに表示

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -8,17 +9,22 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  # 練習1
+    bb_img.set_colorkey((0,0,0))  #四隅を透明に
     tmr = 0
+    x=random.randint(10,1590)
+    y=random.randint(10,890)
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
-
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-
+        screen.blit(bb_img, [x,y])
+        
         pg.display.update()
         clock.tick(1000)
 
