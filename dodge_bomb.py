d1929cff176dbfa634da14934c2cec2eebd05702
tomoya <c011929257@edu.teu.ac.jp>
2023-04-25 14:25:54 +0900

    爆弾を移動させる

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,12 +15,19 @@ def main():
     tmr = 0
     x=random.randint(10,1590)
     y=random.randint(10,890)
-
+    vx=+5 ; vy=+5  # 速度の設定
+    bb_rct=bb_img.get_rect()
+    bb_rct.move_ip(vx,vy) 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
         tmr += 1
+        bb_rct.move_ip(vx,vy)  # 爆弾を動かす
+        if x+10>=1600 or 0>=x-10:
+            vx*=-1
+        if y+10>=900 or 0>=y-10:
+            vy*=-1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
         screen.blit(bb_img, [x,y])
